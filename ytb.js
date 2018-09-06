758b6cbd3e0a0970d4fd55f44361aaa2e92d050c
condef5 <davis.con.fab@gmail.com>
2018-09-06 17:56:14 -0500

    Setup client

--- ytb.js
+++ ytb.js
@@ -1,5 +1,3 @@
-const fetch = require("node-fetch");
-
 const getUrl = (id) => {
   const key =  "AIzaSyCewOz3ojxPLn2ddwmTkeoutd5ORiLA7g8";
   const url = `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${key}&part=snippet,statistics&fields=items(snippet)`;
